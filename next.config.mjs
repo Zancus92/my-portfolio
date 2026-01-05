e2f9ee118888be3a7c9de1b19b00d206bc6d751b
Zanco Simone <zancosimone6@gmail.com>
2026-01-05 19:48:30 +0100

    compressione immagini

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    // Abilitiamo AVIF per la massima compressione (fino al 20% meglio di WebP)
+    formats: ['image/avif', 'image/webp'],
+    // Definiamo le larghezze che Vercel userà per ridimensionare le foto automaticamente
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
 };
 
 export default nextConfig;
